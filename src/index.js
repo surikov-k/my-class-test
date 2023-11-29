1f73f0caab99ba3e24a2b1902c9716b201517e97
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-11-29 11:57:20 +0300

    Added .env file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const {DEFAULT_PORT, HttpCode} = require("./constants");
-
+const port = process.env.PORT || DEFAULT_PORT;
 const mock = require('../mock');
 
 const express = require('express');
@@ -11,5 +11,5 @@ app.get(`/`, (req, res) => res.json(mock));
 app.use((req, res) => res.sendStatus(HttpCode.NOT_FOUND));
 
 app.listen(DEFAULT_PORT, () => {
-  console.log(`Сервер запущен на ${DEFAULT_PORT} порту`);
+  console.log(`Сервер запущен на ${port} порту`);
 });
